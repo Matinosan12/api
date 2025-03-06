66bf9e85575186b04db684b3d8134b8949f8ee83
Matinosan12 <dabaghmatin6@gmail.com>
2025-03-06 16:39:36 +0330

    just change

--- script.js
+++ script.js
@@ -6,12 +6,20 @@
         const postList = document.getElementById('main');
        data.forEach(post => {
             const div = document.createElement('div');
+            const pParent = document.createElement('div');
+            const pParent2 = document.createElement('div');
             const p = document.createElement('p');
             const p2 = document.createElement('p');
-            div.className="post";
+            div.className="col-md-6 d-flex rounded border justify-content-center align-items-center  ";
              p.textContent = `${post.title}`;
              p2.textContent = `${post.body}`;
-            postList.appendChild(div,p,p2);
+             pParent.appendChild(p);
+             pParent2.appendChild(p2);
+             div.appendChild(pParent);
+             div.appendChild(pParent2);
+            postList.appendChild(div);
+            pParent.className="col-md-4"
+            pParent2.className="col-md-8"
          });
 
      }
